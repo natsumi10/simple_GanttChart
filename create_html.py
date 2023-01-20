6b4132459a65ec82994b5ef4db1e7d9ce005be28
natsumi10 <natsumi.k1234@gmail.com>
2023-01-20 15:42:21 +0900

    Start working for create_html.py that drive html file.

---
+++ create_html.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+	''' 
+	Main function 
+	:rtype: int
+	'''
+	read_csv_file()
+	return 0
+
+if __name__ == "__main__":
+	sys.exit(main())
