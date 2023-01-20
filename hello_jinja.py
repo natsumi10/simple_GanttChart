b5da4caf70e24aa79a4795544473a57708fb0e97
natsumi10 <natsumi.k1234@gmail.com>
2023-01-20 23:56:18 +0900

    Start working for create_html.py that drive html file.

--- hello_jinja.py
+++ hello_jinja.py
@@ -2,12 +2,28 @@
 import os
 import sys
 
+import json
+from jinja2 import Environment, FileSystemLoader
+
 def main():
 	''' 
 	Main function 
 	:rtype: int
 	'''
-	
+	"""
+	with open("albums.json", "r") as d:
+		albums = json.load(d)
+	"""
+	fileLoader = FileSystemLoader("templates")
+	env = Environment(loader=fileLoader)
+
+	#rendered = env.get_template("hello_jinja.html").render(albums=albums, title="Gallery")
+	rendered = env.get_template("hello_jinja.html").render( title="Gallery")
+
+	#Write HTML to a file - index.html
+	fileName = "index.html"
+	with open(f"./site/{fileName}","w") as f:
+		f.write(rendered)
 	return 0
 
 if __name__ == "__main__":
