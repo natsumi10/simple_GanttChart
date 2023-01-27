0cc3aeca65f1857a2d18cf6f7ef2466ae7cd1e32
natsumi10 <natsumi.k1234@gmail.com>
2023-01-27 17:34:54 +0900

    Continue work with Flask

--- flask_main.py
+++ flask_main.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from simple_GanttChart import read_csv_file
 from simple_GanttChart import start_simple_ganttchart
+from manage_html.create_html import write_html
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    start_simple_ganttchart()
     return "<p>Hello world!</p>"
 
+def settings():
+    csv_db = read_csv_file()
+    write_html(csv_db)
+    #start_simple_ganttchart()
+    return 0
+
 if __name__ == '__main__':
-   app.run(debug=True, host='0.0.0.0')
+    settings()
+    app.run(debug=True, host='0.0.0.0')
